3eedb6bf6502f86a5b1053c5be73a4b2ca57bd99
Steve Jackson <steve.jxsn@gmail.com>
2016-09-05 15:04:07 -0400

    parse four-nine

--- app/digit.js
+++ app/digit.js
@@ -24,11 +24,53 @@ const three = [
   ," _|"
 ];
 
+const four = [
+   "   "
+  ,"|_|"
+  ,"  |"
+];
+
+const five = [
+   " _ "
+  ,"|_ "
+  ," _|"
+];
+
+const six = [
+   " _ "
+  ,"|_ "
+  ,"|_|"
+];
+
+const seven = [
+   " _ "
+  ,"  |"
+  ,"  |"
+];
+
+const eight = [
+   " _ "
+  ,"|_|"
+  ,"|_|"
+];
+
+const nine = [
+   " _ "
+  ,"|_|"
+  ," _|"
+];
+
 const map = [
   { digit: zero, value: 0 },
   { digit: one, value: 1 },
   { digit: two, value: 2 },
   { digit: three, value: 3 },
+  { digit: four, value: 4 },
+  { digit: five, value: 5 },
+  { digit: six, value: 6 },
+  { digit: seven, value: 7 },
+  { digit: eight, value: 8 },
+  { digit: nine, value: 9 },
 ]
 
 
--- test/digit.js
+++ test/digit.js
@@ -42,6 +42,60 @@ describe('parseDigit', function() {
     expect(parseDigit(three)).to.equal(3);
   });
 
+  it('can parse a four', function() {
+    const four = [
+                 "   "
+                ,"|_|"
+                ,"  |"
+    ];
+    expect(parseDigit(four)).to.equal(4);
+  });
+
+  it('can parse a five', function() {
+    const five = [
+                 " _ "
+                ,"|_ "
+                ," _|"
+    ];
+    expect(parseDigit(five)).to.equal(5);
+  });
+
+  it('can parse a three', function() {
+    const six = [
+                 " _ "
+                ,"|_ "
+                ,"|_|"
+    ];
+    expect(parseDigit(six)).to.equal(6);
+  });
+
+  it('can parse a seven', function() {
+    const seven = [
+                 " _ "
+                ,"  |"
+                ,"  |"
+    ];
+    expect(parseDigit(seven)).to.equal(7);
+  });
+
+  it('can parse a eight', function() {
+    const eight = [
+                 " _ "
+                ,"|_|"
+                ,"|_|"
+    ];
+    expect(parseDigit(eight)).to.equal(8);
+  });
+
+  it('can parse a nine', function() {
+    const nine = [
+                 " _ "
+                ,"|_|"
+                ," _|"
+    ];
+    expect(parseDigit(nine)).to.equal(9);
+  });
+
   it('throws exception on bad input', function() {
     const garbage = [
           "---",
