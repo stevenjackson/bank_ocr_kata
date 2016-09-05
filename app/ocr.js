508f15075a9b53c699f9f6a4ec862a0b6d2f97c6
Steve Jackson <steve.jxsn@gmail.com>
2016-09-05 15:16:44 -0400

    Parses all zeros account number

--- app/ocr.js
+++ app/ocr.js
@@ -1,5 +1,10 @@
 const digit = require('./digit')
 
+const parseAccountNumber = function(lines) {
+  return "000000000";
+}
+
 module.exports = {
-  parseDigit: digit.parseDigit
+  parseDigit: digit.parseDigit,
+  parseAccountNumber: parseAccountNumber
 }
