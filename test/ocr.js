afbfd51eb1515442e65003397ae78ce4a17742c2
Steve Jackson <steve.jxsn@gmail.com>
2016-09-06 21:18:51 -0400

    Finish story 1 - Read hundreds of account numbers

--- test/ocr.js
+++ test/ocr.js
@@ -32,5 +32,10 @@ describe('parseFile', function() {
     accountNumbers = parseFile('test/two_account_sample.txt');
     return expect(accountNumbers).to.eventually.deep.equal(["123456789", "490067715"]);
   });
+
+  it('can read hundreds of account numbers', function() {
+    accountNumbers = parseFile('test/500_account_sample.txt');
+    return expect(accountNumbers).to.eventually.have.length(504);
+  });
 });
 
